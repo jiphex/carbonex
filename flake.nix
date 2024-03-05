84cae06175c52dfbf934672b210b0666b31007f9
James Hannah <git@james.jph.im>
2024-03-05 21:53:30 +0000

    naersk openssl

--- flake.nix
+++ flake.nix
@@ -12,7 +12,11 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ "pkg-config" ];
+          buildInputs = with pkgs; [ openssl ];
+        };
         devShell = with pkgs; mkShell {
           buildInputs = [ pkg-config openssl.dev cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
